0348cdab72f5da58159bbd10e5db3609420a43db
Zayn Hamza <nerovsdante89@gmail.com>
2022-05-24 14:12:48 +0300

    whitenoise

--- muhanned/settings.py
+++ muhanned/settings.py
@@ -137,7 +137,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
